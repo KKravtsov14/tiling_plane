522b06b189d876d185cebc99d0d56fbd652396f8
Andrey <andreiliza1981@mail.ru>
2020-03-19 23:55:57 +0700

    made a hexagon

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ import turtle as t
 import math as m
 
 # программа рисует шестиугольник, закраска цвета, выбор координат
-def hexagon(x, y, side_len, color):
+x = 0
+y = 0
+n = 6
+side_len = 100
+color = 'brown'
 
-    pass
+def hexagon(x, y, side_len, color):
+    t.right(30)
+    for i in range(6):
+        t.forward(side_len)
+        t.left(60)
+    t.left(30)
 
 
 def hexagons_line(n, x, y, side_len, color):
@@ -34,3 +43,4 @@ def hexagons_tiles(n1, n, x, y, side_len, color):
             y_new = y - side_len - (side_len / 2)
             hexagons_tiles(n1 - 1, n, x_new, y_new, side_len, color)
             t.down()
+hexagons_line(n ,x, y, side_len, color)
